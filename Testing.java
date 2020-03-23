60a2572f28a2ebe01b9bf92f5a24a7b0fda2bbd3
heeera <62099287+heeera@users.noreply.github.com>
2020-03-23 23:54:05 +0400

    Add files via upload

--- Testing.java
+++ Testing.java
@@ -6,17 +6,35 @@ import org.junit.Test;
 
 public class Testing {
 	
+	private static final int N = 1000000;
+	private static final int R = 6;
 	@Test
 	public void test() {
+		
+		int powerOfTen = 1;
+		for (int i = 0; i < R; i++)
+			powerOfTen *= 10;
+		
 		Random rgen = new Random();
-		int[] a = new int[1000000];
-		int[] b = new int[1000000];
-		for (int i = 0; i < 1000000; i++)
-			a[i] = b[i] = rgen.nextInt(1000000);
+		int[] a = new int[N];
+		int[] b = new int[N];
+		for (int i = 0; i < N; i++)
+			a[i] = b[i] = rgen.nextInt(powerOfTen);
+		
+		long start = System.currentTimeMillis();
 		Sorting.quickSort(a);
-		Sorting.radixSort(b, 6);
-		for (int i = 0; i < 1000000; i++)
+		long end = System.currentTimeMillis();
+		System.out.println("Quick: " + (end - start));
+		
+		start = System.currentTimeMillis();
+		Sorting.radixSort(b, R);
+		end = System.currentTimeMillis();
+		System.out.println("Radix: " + (end - start));
+		
+		for (int i = 0; i < N; i++)
 			assertEquals(a[i], b[i]);
 	}
 
 }
+
+
